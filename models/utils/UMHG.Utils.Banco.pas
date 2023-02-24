db2273ae76b0d07c082eacac7c6e6c5f873f6310
DESKTOP-S4BD7LK\Ultrabook Lenovo <daniel_thls@yahoo.com.br>
2023-02-24 15:35:23 -0300

    adiciona primeiros arquivos

---
+++ models/utils/UMHG.Utils.Banco.pas
@@ -0,0 +1,21 @@
+unit UMHG.Utils.Banco;
+
+interface
+
+uses
+  FireDac.Comp.Client,
+  FireDAC.Phys.MySQL,
+  FireDac.DApt,
+  FireDAC.Stan.Def,
+  FireDAC.Stan.Async,
+  Data.DB;
+
+type
+  TUtilBanco = class
+  private
+    FConexao: TFDConnection;
+  public
+  end;
+implementation
+
+end.
