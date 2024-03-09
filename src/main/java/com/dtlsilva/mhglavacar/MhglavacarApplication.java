9824b67e8ee666e2159ae411209bb3320af74b18
Daniel Thomaz <daniel_thls@yahoo.com.br>
2024-03-09 15:27:58 -0300

    Commit inicial

---
+++ src/main/java/com/dtlsilva/mhglavacar/MhglavacarApplication.java
@@ -0,0 +1,13 @@
+package com.dtlsilva.mhglavacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhglavacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhglavacarApplication.class, args);
+	}
+
+}
